306061b5c7ddb2b55a27559ef3ffd67f896d875d
Alaa Moussawi <alaamoussawi@gmail.com>
2025-04-08 04:04:14 +0000

    add print statement

--- ollama_functions.py
+++ ollama_functions.py
@@ -1,10 +1,8 @@
 from ollama import embed, chat
 
-
 embeddings = embed(model="deepseek-r1:8b", input=["Here is an example sentence I will be embedding!", "Here's a second one!"])
 
-len(embeddings['embeddings'])
-
+print(len(embeddings['embeddings']))
 
 response = chat(model='deepseek-r1:8b', messages=[
   {
