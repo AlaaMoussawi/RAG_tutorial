4ebe5d72fda82bebe501fcdab2ea22688d4d7b49
Alaa Moussawi <alaamoussawi@gmail.com>
2025-06-09 01:19:58 -0700

    Update 03_04_populate_vector_db.py

--- 03_04_populate_vector_db.py
+++ 03_04_populate_vector_db.py
@@ -2,6 +2,7 @@ import os
 from ollama import embed
 from nltk.tokenize import sent_tokenize
 from database_connect_embeddings import get_psql_session, TextEmbedding
+from sentence_transformers import SentenceTransformer
 
 # import nltk
 # nltk.download("punkt")
@@ -10,6 +11,7 @@ from database_connect_embeddings import get_psql_session, TextEmbedding
 def populate_vector_database(folder_path='all_articles'):
 
     session = get_psql_session()
+    model = SentenceTransformer("SFR-Embedding-Mistral", device="cpu") #https://huggingface.co/Salesforce/SFR-Embedding-Mistral
 
     for filename in os.listdir(folder_path):
         file_path = os.path.join(folder_path, filename)
@@ -21,7 +23,8 @@ def populate_vector_database(folder_path='all_articles'):
                 content = f.read()
             
             sentences = sent_tokenize(content)
-            embeddings = embed(model="custom_deepseek", input=sentences)["embeddings"]
+            # embeddings = embed(model="custom_deepseek", input=sentences)["embeddings"]
+            embeddings = model.encode(sentences)
             
             for i, (embedding, content) in enumerate(zip(embeddings, sentences)):
                 new_embedding = TextEmbedding(embedding=embedding, content=content, file_name=filename, sentence_number=i+1)
