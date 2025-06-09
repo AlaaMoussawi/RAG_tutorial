6fa6972c5afc64704ec74a822c89293ce6d07079
Alaa Moussawi <alaamoussawi@gmail.com>
2025-06-09 01:34:10 -0700

    Update 04_02_prepare_content.py

--- 04_02_prepare_content.py
+++ 04_02_prepare_content.py
@@ -1,7 +1,7 @@
 import sys
-from ollama import embed
 from database_connect_embeddings import get_psql_session, TextEmbedding
 from pull_db_content import search_embeddings, get_surrounding_sentences
+from sentence_transformers import SentenceTransformer
 
 # Identify how many search results till we have 5 matches that generate non-overlapping windows.
 
@@ -37,7 +37,7 @@ def group_entries(entry_ids, file_names, index_of_interest, group_window_size):
 
     # Identify if an entry with index index_of_interest needs grouping with other entries.
 
-    # If it needs no grouping, return an array with just its index (will be hanlded as in get_surrounding_sentences) 
+    # If it needs no grouping, return an array with just its index (will be handled as in get_surrounding_sentences) 
     # If it needs grouping with one or more entries, return array of indices of those entries.
 
     entry_id_of_interest = entry_ids[index_of_interest]
@@ -101,32 +101,27 @@ def get_surrounding_sentences(entry_ids, file_names, group_window_size, session)
         grouped_entries.append(group_entries(entry_ids, file_names, index_of_interest = idx, group_window_size = group_window_size))
 
     combined_groups = consolidate_groupings(grouped_entries)
-
-    # print("Combined groups: ", combined_groups)
-
     min_ids, max_ids = get_min_max_ids(entry_ids, file_names, combined_groups, group_window_size)
-
     surrounding_sentences = []
-    if isinstance(min_ids, list) and isinstance(max_ids, list):
-        for min_id, max_id in zip(min_ids, max_ids):
 
-            surrounding_sentences.append(
-                session.query(TextEmbedding.id, TextEmbedding.sentence_number, TextEmbedding.content, TextEmbedding.file_name)\
-                .filter(TextEmbedding.id >= min_id)\
-                .filter(TextEmbedding.id <= max_id)\
-                .all()
-            )
-        
-        return surrounding_sentences
+    for min_id, max_id in zip(min_ids, max_ids):
+        surrounding_sentences.append(
+            session.query(TextEmbedding.id, TextEmbedding.sentence_number, TextEmbedding.content, TextEmbedding.file_name)\
+            .filter(TextEmbedding.id >= min_id)\
+            .filter(TextEmbedding.id <= max_id)\
+            .all()
+        )
+    
+    return surrounding_sentences
 
 def search_by_query(query, num_matches=5, group_window_size=5):
 
     session = get_psql_session()
-    query_embedding = embed(model="custom_deepseek", input=query)["embeddings"][0]
+    model = SentenceTransformer('SFR-Embedding-Mistral', device='cpu')
+    query_embedding = model.encode(query)
+
     search_results = search_embeddings(query_embedding, session=session, limit=num_matches * (2*group_window_size + 1) )
     filtered_matches = get_filtered_matches(search_results)
-
-    # print(f"Number of filtered matches: {len(filtered_matches)}")
 
     entry_ids = [i[0] for i in filtered_matches]
     file_names = [i[3] for i in filtered_matches]
@@ -137,11 +132,8 @@ def search_by_query(query, num_matches=5, group_window_size=5):
 if __name__=="__main__":
 
     query = "Tell me about children's rights in Germany."
-
-    # filtered_matches = get_filtered_matches(search_results)
     
     if len(sys.argv) > 1:
-        # raise ValueError("Please pass a query when calling the script.")
         query = sys.argv[1]
 
     context = search_by_query(query)
